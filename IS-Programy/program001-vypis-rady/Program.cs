73fca3647e17142aa9787ebaf4f80a57e035534a
David Pavlů <david.pavlu1@tul.cz>
2025-10-01 10:23:16 +0200

    #3 Základ programu pro výpis řady - vstup první číslo

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,27 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**************************");
+    Console.WriteLine("*****Výpis řady čísel*****");
+    Console.WriteLine("**************************");
+    Console.WriteLine("********David Pavlů*******");
+    Console.WriteLine("**************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatné řešení
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu - sprvávné řešení
+    Console.Write("Zadejte první (celé) číslo řady: ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první celé číslo řady znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
