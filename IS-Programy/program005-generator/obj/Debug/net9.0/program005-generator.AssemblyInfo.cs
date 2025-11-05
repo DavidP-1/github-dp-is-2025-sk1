5b006ab0f823df83ef2a568777018ae2d4f15366
David Pavlů <david.pavlu1@tul.cz>
2025-11-05 09:53:47 +0100

    #8 kopie základního kódu generátoru náhodných čísel

--- IS-Programy/program005-generator/obj/Debug/net9.0/program005-generator.AssemblyInfo.cs
+++ IS-Programy/program005-generator/obj/Debug/net9.0/program005-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4336419bc02a7627ff15e10c141828835133e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf73cebefebe3a2bca9cfecb1230ee4b57cd822")]
 [assembly: System.Reflection.AssemblyProductAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program005-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
