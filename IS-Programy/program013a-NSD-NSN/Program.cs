2ff443191751dfe7ca19f8a6100c3832fd07e5ab
David Pavlů <david.pavlu1@tul.cz>
2025-12-03 10:29:36 +0100

    #16 metoda pro načtení čísel

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -3,22 +3,9 @@ while (again == "a")
 {
     Console.Clear();
     razitko();
-
-    Console.Write("Zadejte první číslo (celé číslo): ");
-    int a;
-    while (!int.TryParse(Console.ReadLine(), out a))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
-    }
-
-    Console.Write("Zadejte druhé číslo (celé číslo): ");
-    int b;
-    while (!int.TryParse(Console.ReadLine(), out b))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
-    }
-
-
+    //Načítání hodnot
+    ulong a = nactiCislo("Zadejte číslo a: ");
+    ulong b = nactiCislo("Zadejte číslo b: ");
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
@@ -37,3 +24,21 @@ static void razitko()
     Console.WriteLine("*******************************************");
     Console.WriteLine();
 }
+
+//metoda pro načtení čísel
+static ulong nactiCislo(string zprava)
+{
+    Console.WriteLine(zprava);
+    ulong cislo;
+    while (!ulong.TryParse(Console.ReadLine(), out cislo))
+    {
+        Console.Write("Nezadali jste přirozené číslo. Zadejte číslo znovu: ");
+    }
+
+    // metoda, ketrá vrací nějaký konkrétní datový typ, musí obsahovat tento řádek
+    return cislo;
+
+}
+
+
+
